34e3eb7cde908fe4289b6f30888fd440462a473a
Arash Shafiee <i.arash.shafiee@gmail.com>
2025-02-24 19:19:34 +0330

    wip: mui plugin

--- playground/src/test-page.tsx
+++ playground/src/test-page.tsx
@@ -1,9 +1,35 @@
 import { FB } from "./form-builder";
+import { dispatchArrayAction } from "@mfb/core";
+
+type TestPageForm = {
+  akbar: Array<Record<`input-${1 | 2 | 3}`, string>>;
+};
 
 const TestPage = () => {
   return (
     <>
       <h3>Microkernel From Builder</h3>
+
+      <button
+        onClick={() => {
+          dispatchArrayAction<TestPageForm>(
+            {
+              type: "append",
+              params: [
+                {
+                  "input-1": "value 1",
+                  "input-2": "value 2",
+                  "input-3": "value 3",
+                },
+                {shouldFocus: false,}
+              ],
+            },
+            "akbar"
+          );
+        }}
+      >
+        APPEND TO AKBAR
+      </button>
       <FB.BasicBuilder
         gridContainerProps={{
           spacing: 2,
