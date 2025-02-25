8a7f8ad9144af3863a94797536e12e5d81bda8e8
Arash Shafiee <i.arash.shafiee@gmail.com>
2025-02-25 18:10:46 +0330

    wip: mui plugin

--- playground/src/form-builder/input/text-field.tsx
+++ playground/src/form-builder/input/text-field.tsx
@@ -3,11 +3,11 @@ import type { FieldValues, Path } from "react-hook-form";
 import { Controller, useFormContext } from "react-hook-form";
 import TextField, { type TextFieldProps } from "@mui/material/TextField";
 
-const TextFieldInput = <TData extends FieldValues>({
+const TextFieldInput = <TFields extends FieldValues>({
   name,
   textFieldProps,
 }: {
-  name: Path<TData>;
+  name: Path<TFields>;
   textFieldProps?: Omit<
     TextFieldProps,
     "onChange" | "onBlur" | "disabled" | "value"
